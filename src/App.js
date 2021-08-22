c4ccd297b1fbe5c17cdc65703cc287c65242acc8
Rula Al-qasem <jaradatrula6@gmail.com>
2021-08-22 20:48:27 +0300

    lab 36

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Products from './components/Products'
+import Categories from './components/Categories'
+import Container from '@material-ui/core/Container';
+import store from './store/index.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Provider store={store()}>
+        <Header></Header>
+        <Categories></Categories>
+        <Products></Products>
+        <Footer></Footer>
+      </Provider>
+    </Container>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.scss';
+
+
+
+function Footer() {
+  return (
+    <footer className="App-footer" >
+      <p>
+        Copyright &copy; 2021
+      </p>
+
+    </footer>
+  )
+}
+
+export default Footer;
