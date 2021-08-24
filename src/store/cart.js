8340f7479f0740d54347bdd024ec9ff48df70656
Rula Al-qasem <jaradatrula6@gmail.com>
2021-08-24 10:55:55 +0300

    ch

--- src/store/cart.js
+++ src/store/cart.js
@@ -10,14 +10,20 @@ export default function CategoriesReducer(state = initialState, action) {
 
   switch (type) {
     case "ADD_TO_CART":
-      payload.count++
+
       if (!state.cart.includes(payload)) {
-
+        payload.count++
         newCart = [...state.cart, payload];
+        payload.inStock--
 
-      } else {
+      }
+      else if (payload.inStock == 0) {
+        alert("there is no more from this product")
+      }
+      else {
         newCart = [...state.cart]
-
+        payload.count++
+        payload.inStock--
       }
       const count = state.count + 1;
 
@@ -25,7 +31,11 @@ export default function CategoriesReducer(state = initialState, action) {
 
     case "REMOVE_FROM_CART":
       payload.count--
-      if (payload.count === 0) { newCart2 = state.cart.filter((p) => p !== payload) }
+      payload.inStock++
+      if (payload.count === 0) {
+        newCart2 = state.cart.filter((p) => p !== payload);
+
+      }
       else { newCart2 = [...state.cart] }
       return {
         cart: newCart2,
@@ -34,6 +44,7 @@ export default function CategoriesReducer(state = initialState, action) {
       };
 
     case "REMOVE":
+      payload.inStock = payload.inStock + payload.count
       payload.count = 0
       let c = state.cart.filter((p) => p !== payload)
 
